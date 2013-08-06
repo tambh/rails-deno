6be4d833709b75e36266286fc82a995679496822
tambh <tam@cpu-tambh.(none)>
2013-08-06 16:21:07 +0700

    Initial

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,8 @@
+class PostsController < ApplicationController
+  def new
+    @message = "Hello, how are you today?"
+  end
+
+  def create    
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = 'c62a07656760db8221ff05843d24fb7cc60e36959460bea36c3168cc44c8c1abb6141c6ce65f739d930c49639e5d1e77f7e4ef1ba755805e0d5b52bad3665c9f'
