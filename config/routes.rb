aa097a9ea8bfaa7e9392e769fb5dadff096801ee
Bui Huu Tam <tam@cpu-tambh.(none)>
2013-08-09 15:25:58 +0700

    sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Demo::Application.routes.draw do
   resources :users
-
+  resources :sessions, only: [:new, :create, :destroy]  
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 
   get "welcome/index"
 
